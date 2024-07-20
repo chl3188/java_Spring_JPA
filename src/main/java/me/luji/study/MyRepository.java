405b21f5c7cd8e35f69bea91215412f7ed7b30b6
luji <chl3188@gmail.com>
2024-07-20 21:00:22 +0900

    스프링 데이터 Common Null 처리

--- src/main/java/me/luji/study/MyRepository.java
+++ src/main/java/me/luji/study/MyRepository.java
@@ -2,9 +2,13 @@ package me.luji.study;
 
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
+import javax.swing.text.html.Option;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface MyRepository<T, Id extends Serializable> extends Repository<T, Id> {
@@ -14,4 +18,7 @@ public interface MyRepository<T, Id extends Serializable> extends Repository<T, 
     List<T> findAll();
 
     long count();
+
+    @Nullable
+    <E extends T> Optional <E> findById(Id id);
 }
