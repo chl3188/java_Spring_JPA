0b93df4c2481dc9d69f834e340ed34a286f444b5
luji <chl3188@gmail.com>
2024-07-22 14:01:46 +0900

    스프링 데이터 Common 비동기 쿼리 메소드

--- src/main/java/me/luji/study/CommentRepository.java
+++ src/main/java/me/luji/study/CommentRepository.java
@@ -2,8 +2,10 @@ package me.luji.study;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
@@ -13,8 +15,11 @@ public interface CommentRepository extends MyRepository<Comment, Long> {
     List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountDesc(String comment);
 //
 //    Page<Comment> findByCommentContainsIgnoreCase(String comment, Pageable page);
+//
+//    Stream<Comment> findByCommentContainsIgnoreCase(String comment, Pageable page);
 
-    Stream<Comment> findByCommentContainsIgnoreCase(String comment, Pageable page);
+    @Async
+    Future<List<Comment>> findByCommentContainsIgnoreCase(String comment, Pageable page);
 
     Page<Comment> findByLikeCountGreaterThanAndPost(int likeCnt, Post post, Pageable page);
 }
